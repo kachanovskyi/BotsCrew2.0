86f75e201d2170fb12e78ca2f17a3bc84925acb1
Vova Kachanovskyi <vova.kachanovsky@gmail.com>
2016-11-11 12:32:46 +0200

    added progress bar, created gallery for slide 6 and working on styles

---
+++ js/index.js
@@ -0,0 +1,19 @@
+//progress bar
+var bar = new ProgressBar.Line('#progressbar-container', {
+    strokeWidth: 1,
+    easing: 'easeInOut',
+    duration: 20000,
+    color: '#2F80ED',
+    trailColor: '#DFDFDF',
+    trailWidth: 6,
+    svgStyle: {width: '100%', height: '100%'}
+});
+
+var progressBarInit = function () {
+    bar.animate(1.0, function() {
+        bar.animate(0.0, {duration: 3000}, function () {
+            progressBarInit();
+        });
+    });
+};
+progressBarInit();
