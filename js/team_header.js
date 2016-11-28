fc8aab810daa5a42217bc7ca0272bc1ff7a66c30
Vova Kachanovskyi <vova.kachanovsky@gmail.com>
2016-11-28 15:34:28 +0200

    some fixes on team.html

--- js/team_header.js
+++ js/team_header.js
@@ -1,20 +1,24 @@
-var targetOffset = ($('.content').offset().top - 44*2);
+var targetOffset = $('.content').offset().top;
 console.log(targetOffset, 'targetOffset');
 var $w = $(window).scroll(function(){
-    if ( $w.scrollTop() > targetOffset ) {
+    if ( $w.scrollTop() > (targetOffset - 44*2)) {
         console.log('header styles updated');
         $('.menu-item').addClass('standard-menu-item');
         $('.header .menu-item.bot-btn').removeClass('white');
         $('.header .menu-item.bot-btn').addClass('white-blue');
         $('.logo').attr('src', "../img/logo.svg");
-        $('.navbar').css('background', '#FFFFFF');
-        $('.navbar-default').css('border-bottom', '1px solid #E5E5E5');
     } else {
         $('.menu-item').removeClass('standard-menu-item');
         $('.header .menu-item.bot-btn').removeClass('white-blue');
         $('.header .menu-item.bot-btn').addClass('white');
         $('.logo').attr('src', "../img/logo_white.svg");
-        $('.navbar').css('background', 'transparent');
+    }
+
+    if ( $w.scrollTop() > targetOffset - 44) {
+        $('.navbar-default').css('border-bottom', '1px solid #E5E5E5');
+        $('.navbar').css('background', '#FFFFFF');
+    } else {
         $('.navbar-default').css('border-bottom', 'none');
+        $('.navbar').css('background', 'transparent');
     }
 });
